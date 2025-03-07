41c6ec2eddea4515d691300d09045d5351e4630e
siiraj0 <sirajkp752@gmail.com>
2025-03-07 20:18:57 +0530

    updated

--- app.js
+++ app.js
@@ -1,23 +1,69 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "iam sirajudheen kp from malappuram "),
-    React.createElement("h2", {}, "this is my react practice"),
-  ])
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <div id="logo-container">
+        <img
+          className="logo"
+          src="https://marketplace.canva.com/EAFaFUz4aKo/2/0/1600w/canva-yellow-abstract-cooking-fire-free-logo-JmYWTjUsE-Q.jpg"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "hello this sirajudheen kp"
-);
+const RestuarentCard = (props) => {
+  console.log(props);
+  
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/e0vvulfbahjxjz6k4uwi"
+      />
+      <h3>{props.resName}</h3>
+      <h4>{props.Cuisine}</h4>
+      <h4>⭐ 4.5</h4>
+      <h4>⏳ 25 min</h4>
+    </div>
+  );
+};
 
-console.log(heading);
+const Body = () => {
+  return (
+    <div className="Body">
+      <div className="Search">Search</div>
+      <div className="res-container">
+        <RestuarentCard
+          resName="Meghna Food"
+          Cuisine="Biriyani, North Indian . Asian"
+        />
+        <RestuarentCard resName="KFC" Cuisine="Burger , Fast Food" />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<AppLayout />);
