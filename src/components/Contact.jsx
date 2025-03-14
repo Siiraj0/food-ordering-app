28f8af6810718f9def17b948cfa014b3c9650a8c
siiraj0 <sirajkp752@gmail.com>
2025-03-14 20:49:56 +0530

    updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ const Contact = () => {
         <div>
             <h1>Contact page is here</h1>
             <h1>ph:9738473749</h1>
-       
+            <h1>foofie@gmail.com</h1>
         </div>
     )
 }
