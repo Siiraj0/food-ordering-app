843f96b2e3b0fe49c320dd049999d52ec760eb42
siiraj0 <sirajkp752@gmail.com>
2025-03-18 21:55:21 +0530

    updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,16 +1,12 @@
 const Contact = () => {
-
-
-    
-    return (
-        <div>    
-            <h1>Contact page is here</h1>
-            <h1>ph:9738473749</h1>
-            <h1>foofie@gmail.com</h1>
-            <h1>malappuram </h1>    
-        </div>
-    )
-}
-     
+  return (
+    <div>
+      <h1>Contact page is here</h1>
+      <h1>ph:9738473749</h1>
+      <h1>foofie@gmail.com</h1>
+      <h1>malappuram </h1>
+    </div>
+  );
+};
 
 export default Contact;
